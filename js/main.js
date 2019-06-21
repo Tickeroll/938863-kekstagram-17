396c77f7419f6a5c44e2921fec84a361ce60c1c8
Tickeroll <revelationwind@gmail.com>
2019-06-21 20:50:54 +0300

    Задание 4

--- js/main.js
+++ js/main.js
@@ -44,3 +44,105 @@ for (var i = 0; i < photos.length; i++) {
   node.querySelector('.picture__comments').textContent = photos[i].comments.length;
   photoBlockElement.appendChild(node);
 }
+var uploadForm=document.querySelector('.img-upload__overlay');
+var formEscape=document.getElementById('upload-cancel');
+var scaleControlValue=document.querySelector('.scale__control--value');
+
+document.querySelector('#upload-file').onchange= function(){
+uploadForm.classList.remove('hidden');
+};
+
+document.getElementsByTagName('body')[0].onkeyup=function(event){
+  if (event.key === "Escape"){
+    uploadForm.classList.add('hidden');
+  }
+};
+
+formEscape.onclick=function(){
+  uploadForm.classList.add('hidden');
+};
+
+document.querySelector('.scale__control--smaller').onclick=function(){
+var percent=parseInt(scaleControlValue.value);
+  percent-=25;
+  if (percent > 0) {
+    scaleControlValue.value = percent+"%";
+    scaleControlValue.onchange();
+  };
+}
+document.querySelector('.scale__control--bigger').onclick=function(){
+var percent=parseInt(scaleControlValue.value);
+  percent+=25;
+  if (percent < 100) {
+    scaleControlValue.value = percent +"%";
+    scaleControlValue.onchange();
+  }
+}
+scaleControlValue.onchange = function () {
+  document.querySelector('.img-upload__preview').style.transform='scale(0.'+ parseInt(scaleControlValue.value)+")";
+}
+var effectChanges=document.querySelectorAll('.effects__radio');
+var currentEffect="";
+var effectLevel = document.querySelector('.effect-level__value');
+var effectPin = document.querySelector('.effect-level__pin')
+
+for (var i = 0; i < effectChanges.length; i++){
+effectChanges[i].onclick=function(){
+  currentEffect=this.id;
+
+  document.querySelector('.img-upload__preview img');
+  if (this.id == 'effect-none'){
+    document.querySelector('.img-upload__preview img').className = 'effects__preview--none';
+    document.getElementsByClassName('img-upload__effect-level')[0].classList.add('hidden');
+    }else if (this.id == 'effect-chrome')
+    {
+    document.querySelector('.img-upload__preview img').className = 'effects__preview--chrome';
+    document.getElementsByClassName('img-upload__effect-level')[0].classList.remove('hidden');
+    }else if (this.id == 'effect-sepia')
+    {
+    document.querySelector('.img-upload__preview img').className = 'effects__preview--sepia';
+    document.getElementsByClassName('img-upload__effect-level')[0].classList.remove('hidden');
+    }else if (this.id == 'effect-marvin')
+    {
+    document.querySelector('.img-upload__preview img').className = 'effects__preview--marvin';
+    document.getElementsByClassName('img-upload__effect-level')[0].classList.remove('hidden');
+    }else if (this.id == 'effect-phobos')
+    {
+    document.querySelector('.img-upload__preview img').className = 'effects__preview--phobos';
+    document.getElementsByClassName('img-upload__effect-level')[0].classList.remove('hidden');
+    }else if (this.id == 'effect-heat')
+    {
+    document.querySelector('.img-upload__preview img').className = 'effects__preview--heat';
+    document.getElementsByClassName('img-upload__effect-level')[0].classList.remove('hidden');
+    }
+    effectPin.style.left='100%';
+    effectPin.onmouseup();
+  }
+}
+document.querySelector('.effect-level__pin').onmouseup = function (event) {
+  effectLevel.value = parseInt(this.style.left);
+  effectLevel.onchange();
+  }
+effectLevel.onchange = function() {
+   if (currentEffect == 'effect-none'){
+    document.querySelector('.img-upload__preview').style.filter = "";
+    }else if (currentEffect == 'effect-chrome')
+    {
+    document.querySelector('.img-upload__preview').style.filter = 'grayscale('+ (this.value/100)+')';
+    }else if (currentEffect == 'effect-sepia')
+    {
+    document.querySelector('.img-upload__preview').style.filter = 'sepia('+ (this.value/100)+')';
+    }else if (currentEffect == 'effect-marvin')
+    {
+    document.querySelector('.img-upload__preview').style.filter = 'invert('+ (this.value)+'%)';
+    }else if (currentEffect == 'effect-phobos')
+    {
+    document.querySelector('.img-upload__preview').style.filter = 'blur('+ (this.value/100*3)+'px)';
+    }else if (currentEffect == 'effect-heat')
+    {
+    document.querySelector('.img-upload__preview').style.filter = 'brightness('+ (this.value/100*3)+')';
+}
+console.log(document.querySelector('.img-upload__preview').style.filter);
+}
+document.querySelector('.img-upload__preview')
+document.querySelector('.effect-level__pin').offsetWidth;
