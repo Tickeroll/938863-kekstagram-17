2133dac69734fbca95e9c3c4fba0c7712bdf11dc
Tickeroll <revelationwind@gmail.com>
2019-06-30 18:35:19 +0300

    Решение задачи 6-14 надо подкачаться

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,19 @@
 'use strict';
 (function () {
   var template = document.querySelector('#picture');
-  var photos = window.generatePhotoList();
   var photoBlockElement = document.querySelector('.pictures');
+  window.getData(function (photos) {
+    /**
+     *Добавляем в разметку блоки с фтографиями и описанием
+     */
+    for (var i = 0; i < photos.length; i++) {
+      var node = document.importNode(template.content, true);
+      node.querySelector('img').src = photos[i].url;
+      node.querySelector('.picture__likes').textContent = photos[i].likes;
+      node.querySelector('.picture__comments').textContent = photos[i].comments.length;
+      photoBlockElement.appendChild(node);
+    }
+  });
 
-  /**
-   *Добавляем в разметку блоки с фтографиями и описанием
-   */
-  for (var i = 0; i < photos.length; i++) {
-    var node = document.importNode(template.content, true);
-    node.querySelector('img').src = photos[i].url;
-    node.querySelector('.picture__likes').textContent = photos[i].likes;
-    node.querySelector('.picture__comments').textContent = photos[i].comments.length;
-    photoBlockElement.appendChild(node);
-  }
+
 })();
